a35477e4897d3089ea9b6caf0cf0386eaa6770b7
Stefano Paulo <stefanopaulo47@gmail.com>
2020-07-10 14:24:52 -0300

    estruturas de controle

--- 02-iniciando-do-zero-com-php/02-07-estruturas-de-controle/index.php
+++ 02-iniciando-do-zero-com-php/02-07-estruturas-de-controle/index.php
@@ -9,6 +9,35 @@ fullStackPHPClassName("02.07 - Estruturas de controle");
  */
 fullStackPHPClassSession("if, elseif, else", __LINE__);
 
+$test = true;
+
+if ($test) {
+    var_dump(true);
+} else {
+    var_dump(false);
+}
+
+$age = 16;
+
+if ($age < 20) {
+    var_dump("Bandas Coloridas");
+} elseif ($age < 50) {
+    var_dump("Ótimas Bandas");
+} else {
+    var_dump("Rock and Roll Raíz");
+}
+
+$hour = 15;
+
+if ($hour >= 5 && $hour < 23) {
+    if ($hour < 7) {
+        var_dump("Bob Maley");
+    } else {
+        var_dump("Alter Bridge");
+    }
+} else {
+    var_dump("ZzzzzzzZZZZ");
+}
 
 /*
  * [ isset ] https://php.net/manual/pt_BR/function.isset.php
@@ -16,12 +45,51 @@ fullStackPHPClassSession("if, elseif, else", __LINE__);
  */
 fullStackPHPClassSession("isset, empty, !", __LINE__);
 
+$rock = "";
+
+// verifica se a variável existe
+if (isset($rock)) {
+    var_dump("Rock And Roll!");
+} else {
+    var_dump("Die");
+}
+
+$rockAndRoll = "AC/DC";
+
+// verifica se a variável existe e tem valor
+if (!empty($rockAndRoll)) {
+    var_dump("Rock existe e toca {$rockAndRoll}");
+} else {
+    var_dump("Não existe ou não está tocando!");
+}
 
 /*
  * [ switch ] https://secure.php.net/manual/pt_BR/control-structures.switch.php
  */
 fullStackPHPClassSession("switch", __LINE__);
 
+$payment = "past_due";
 
+switch ($payment) {
+    case "billet_printed":
+        var_dump("Boleto Impresso");
+    break;
 
+    case "canceled":
+        var_dump("Pagamento Cancelado");
+    break;
+    
+    case "past_due":
+    case "pending":
+        var_dump("Aguardando Pagamento");
+    break;
+    
+    case "approved":
+    case "completed":
+        var_dump("Pagamento Aprovado");
+    break;
 
+    default:
+        var_dump("Erro ao processar pagamento!");
+    break;
+}
