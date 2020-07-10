b4a08b92456506f54c5fc842eea7282777f49063
Stefano Paulo <stefanopaulo47@gmail.com>
2020-07-10 10:34:06 -0300

    iniciando do zero com php

---
+++ 01-ola-mundo-vamos-comecar/01-06-iniciando-um-projeto/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>FSPHP: Iniciando Um Projeto!</title>
+
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <?php 
+        /**
+         * Bloco PHP com variável start
+         * comando de saída!
+         */
+        $start = "Vamos Começar!";
+        echo "<h1>Olá Mundo! {$start}</h1>";
+        echo "<p id='js'>Loading...</p>";
+    ?>
+
+    <script src="assets/scripts.js"></script>
+</body>
+</html>
